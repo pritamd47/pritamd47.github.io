218b38f2991d405b7b781f37df13b8bc05e51ca7
pritamd47 <pdas47@uw.edu>
2025-05-25 07:49:23 +0000

    Deploying to gh-pages from @ pritamd47/pritamd47.github.io@36ed68800974ef1aa33285653ddeca49b3ad3b62 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
