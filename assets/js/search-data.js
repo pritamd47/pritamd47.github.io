fa022247e30cc8e72b5fa3fdbe256ec7288ba27b
pritamd47 <pdas47@uw.edu>
2025-05-26 09:30:03 +0000

    Deploying to gh-pages from @ pritamd47/pritamd47.github.io@be5d1bb0c69d602e27e23cf947ebc817429fffce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,62 +37,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-reservoir-assessment-tool-rat-mekong",
+        },{id: "projects-reservoir-assessment-tool-rat-mekong",
           title: 'Reservoir Assessment Tool (RAT) - Mekong',
           description: "Remote Sesnsing and Hydrologic Modeling enables tracking of reservoir operations and water availability in the Mekong River Basin.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/rat-mekong/";
+            },},{id: "projects-plotting-large-climate-data-interactively",
+          title: 'Plotting large climate data interactively',
+          description: "Climate data can be large. Not just climate data, similar datasets – satellite remote sensing, lidar, model data - all can be large, and difficult to work with, including visualizing them performantly. In this project, I explore different ways to visualize large data, with the goal to let users interact with the underlying large dataset in a performant way.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/siparcs-interactive-vis/";
             },},{
         id: 'social-email',
         title: 'email',
